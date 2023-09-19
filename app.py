f8b16bb2a8e1f6887c9b45dc330dde54b7bf1ee6
Bagus Pramudya <bagusesap@gmail.com>
2023-09-19 23:45:11 +0700

    change into debug and add lib
    add poppler-utils and change app.debug into True

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def count_matched_sections(text, input_sections):
     return matched_sections
 
 if __name__ == '__main__':
-    app.debug=False
+    app.debug=True
     app.run(host='0.0.0.0', port=5000)  # Listen on all available network interfaces
